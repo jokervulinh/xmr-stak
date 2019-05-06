04d3f4f878977212f23e0ed9e782c5b91af14a1b
jokervulinh <jokervulinh@gmail.com>
2019-05-06 16:40:01 +0700

    Update cli-miner.cpp

--- xmrstak/cli/cli-miner.cpp
+++ xmrstak/cli/cli-miner.cpp
@@ -198,7 +198,7 @@ inline void prompt_once(bool& prompted)
 inline bool use_simple_start()
 {
 	// ask this question only once
-	static bool simple_start = read_yes_no("\nUse simple setup method? (Y/n)", "Y\n");
+	static bool simple_start = read_yes_no("\nUse simple setup method? (Y/n)", "yes\n");
 	return simple_start;
 }
 
